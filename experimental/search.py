dd057420e0ca4394c478fa58bcca57d16333ff5a
Jason J. Gullickson <jgullickson@2soc.net>
2017-03-19 10:34:53 -0500

    Sending search term as params to requests module doesn\'t seem to work as expected, falling-back to URL string munging.

--- experimental/search.py
+++ experimental/search.py
@@ -4,21 +4,27 @@ import json
 import requests
 from requests.auth import HTTPBasicAuth
 
-searchUrl = 'https://api.github.com/search/code'
+#searchUrl = 'https://api.github.com/search/code'
 
 # get auth from user
 githubUser = raw_input('github username: ')
 githubPassword = raw_input('github password: ')
 searchTerm = raw_input('what to search for: ')
 
-searchParams = dict(
-    q='%s+extension:stl' % searchTerm
-)
+searchUrl = 'https://api.github.com/search/code?q=%s+in:path+extension:stl' % searchTerm
 
-searchResponse = requests.get(url=searchUrl, auth=HTTPBasicAuth(githubUser, githubPassword), params=searchParams)
+#searchParams = dict(
+#    q='%s+in:path+extension:stl' % searchTerm 
+#)
+
+searchResponse = requests.get(url=searchUrl, auth=HTTPBasicAuth(githubUser, githubPassword))
 searchData = json.loads(searchResponse.text)
 
-if searchData['items']:
+print 'request status: %d' % searchResponse.status_code
+
+if searchResponse.status_code > 199 and searchResponse.status_code < 300:
+    # TODO: make sure results collection exists
+    print '%d matching results' % searchData['total_count']
     for searchResult in searchData['items']:
         print searchResult['name']
         print searchResult['repository']['name']
@@ -26,4 +32,4 @@ if searchData['items']:
         print searchResult['repository']['html_url']
         print ''
 else:
-    print searchData
+    print '%d error while searching: %s' % (searchResponse.status_code, response.text)
