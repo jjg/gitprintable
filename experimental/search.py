ae9458912880dc8b3172f6b29bec10b3e1d439f6
Jason J. Gullickson <jgullickson@2soc.net>
2017-03-19 10:09:07 -0500

    Search script prompts for terms, filters STLs properly.

--- experimental/search.py
+++ experimental/search.py
@@ -9,14 +9,21 @@ searchUrl = 'https://api.github.com/search/code'
 # get auth from user
 githubUser = raw_input('github username: ')
 githubPassword = raw_input('github password: ')
+searchTerm = raw_input('what to search for: ')
 
 searchParams = dict(
-    q='stl+in:extension'
+    q='%s+extension:stl' % searchTerm
 )
 
 searchResponse = requests.get(url=searchUrl, auth=HTTPBasicAuth(githubUser, githubPassword), params=searchParams)
 searchData = json.loads(searchResponse.text)
 
-for searchResult in searchData['items']:
-    print searchResult['name']
-    print searchResult['repository']['description']
+if searchData['items']:
+    for searchResult in searchData['items']:
+        print searchResult['name']
+        print searchResult['repository']['name']
+        print searchResult['repository']['description']
+        print searchResult['repository']['html_url']
+        print ''
+else:
+    print searchData
